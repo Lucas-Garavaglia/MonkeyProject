4aa916a1d5d80c87c444892d7084a039f43fe709
lucas-garavaglia <lucasgrafimar@gmail.com>
2021-03-05 12:48:13 -0300

    Alteração no  rank

--- commands/rank.js
+++ commands/rank.js
@@ -5,14 +5,20 @@ module.exports = {
 	description: "Mostra o rank geral.",
 	execute(message, args, db) {
 		var Embed = new MessageEmbed();
+		var experience = 0;
 		Embed.setColor("#F0F0FF")
 			.setTitle(`Ranking`)
 			.setDescription("Mostra o raking");
 		db.all(`SELECT * FROM Users ORDER BY level DESC,xp DESC;`, (err, rows) => {
 			for (var i = 0; i < rows.length; i++) {
+				for (var j = 0, experience = 0; j < rows[i].level; j++) {
+					experience = experience + Math.pow((j + 1) * 4, 2);
+				}
 				Embed.addField(
 					`(${i + 1}) ${rows[i].name}`,
-					`Level ${rows[i].level}\nXp ${rows[i].xp}`
+					`Level ${rows[i].level}\n
+					 Xp ${rows[i].xp}\n
+					 Total Xp ${experience}`
 				);
 			}
 			message.channel.send(Embed).catch(console.error);
